d68072334cf49246099fd8cf54e9b2e5da81120c
angruwl <grigorova07nastya@gmail.com>
2026-02-09 09:07:54 -0800

    correction enter empty

--- lab #1/#1.py
+++ lab #1/#1.py
@@ -11,7 +11,7 @@ def count_meet(str):
     return ",".join(single_char)
 while True:
     a=input("Введите строку: ")
-    if not a or not len(a)<1:
+    if not a or len(a)==0:
         print("Недостаточно символов!")
         continue
     if not a[-1]=='.':
